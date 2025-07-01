26c204512cf7a861ac18b1a198c58148c3d1d557
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2025-07-01 11:24:16 +0200

    update conf

--- content/conf.py
+++ content/conf.py
@@ -17,8 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "LESSON NAME"
-copyright = "2021, The contributors"
+project = "Python for High Performace Data Analysis"
+copyright = "2025, The contributors"
 author = "The contributors"
 github_user = "ENCCS"
 github_repo_name = ""  # auto-detected from dirname if blank
